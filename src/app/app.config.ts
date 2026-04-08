2169f219d54eec7089b4adccfff7c314f5c7b07f
Augusto0414 <augustopelis4@gmail.com>
2026-04-08 17:42:16 -0500

    feat: update provideIcons

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,24 +2,16 @@ import { HTTP_INTERCEPTORS, provideHttpClient, withFetch } from '@angular/common
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
-import { NgIconsModule } from '@ng-icons/core';
+import { provideIcons } from '@ng-icons/core';
 import {
-  featherAlertCircle as iconAlertCircle,
-  featherArchive as iconArchive,
-  featherCheck as iconCheck,
-  featherEdit as iconEdit,
-  featherX as iconX
+  featherAlertCircle,
+  featherArchive,
+  featherCheck,
+  featherEdit,
+  featherX
 } from '@ng-icons/feather-icons';
 import { routes } from './app.routes';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
-
-const ngIconsConfig = NgIconsModule.withIcons({ 
-  featherEdit: iconEdit,
-  featherCheck: iconCheck,
-  featherX: iconX,
-  featherArchive: iconArchive,
-  featherAlertCircle: iconAlertCircle
-});
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -27,7 +19,13 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideAnimations(),
-    ...(ngIconsConfig.providers || []),
+    provideIcons({
+      featherEdit,
+      featherCheck,
+      featherX,
+      featherArchive,
+      featherAlertCircle
+    }),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: ErrorInterceptor,

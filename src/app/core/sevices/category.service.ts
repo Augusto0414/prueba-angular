3a72e3cc8c44eb9caa1d01c941eaf153a624369a
Augusto0414 <augustopelis4@gmail.com>
2026-04-08 12:05:53 -0500

    feat: creation of service category

--- src/app/core/sevices/category.service.ts
+++ src/app/core/sevices/category.service.ts
@@ -1,8 +1,46 @@
 import { Injectable } from '@angular/core';
-
+import { Observable, of } from 'rxjs';
+import { Category } from '../../features/categories/models/category.model';
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryService {
-  
+  private categories: Category[] = [
+    {
+      id: '1',
+      code: 'CAT-001',
+      name: 'Tecnología',
+      description: 'Productos tech',
+      isActive: true
+    }
+  ];
+
+  getCategories(): Observable<Category[]> {
+    return of(this.categories);
+  }
+
+  createCategory(category: Category): Observable<Category> {
+    const newCategory = { ...category, id: (this.categories.length + 1).toString() };
+    this.categories = [...this.categories, newCategory];
+    return of(newCategory);
+    }
+
+    updateCategory(updatedCategory: Category): Observable<Category> {  
+    this.categories = this.categories.map(category =>
+      category.id === updatedCategory.id ? updatedCategory : category
+    );
+    return of(updatedCategory);
+  }
+  toggleStatus(categoryId: string): Observable<Category> {
+    const category = this.categories.find(cat => cat.id === categoryId);
+    if (category) category.isActive = !category.isActive;
+    return of();
+  }
+
+  deleteCategory(categoryId: string): Observable<void> {
+      this.categories = this.categories.filter(cat => cat.id !== categoryId);
+      return of();
+  }
 }
+
+
